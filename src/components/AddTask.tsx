d527b623db102b6e1a9b84e551e41b31555354a9
PeSzpak <dropszpalves@gmail.com>
2025-12-10 09:38:37 -0300

    add task management components and configure Tailwind CSS

---
+++ src/components/AddTask.tsx
@@ -0,0 +1,7 @@
+export function AddTask() {
+    return (
+        <>
+            <h1>Adicionar Tarefa</h1>
+        </>
+    )
+}
---
+++ src/components/tasks.tsx
@@ -0,0 +1,7 @@
+export function Tasks() {
+    return (
+        <>
+            <h1>EM CONTRUÇÃO...</h1>
+        </>
+    )
+}
